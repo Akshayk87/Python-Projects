b3f0d1472b69b774818cc55ce930a4730318cdc2
AKSHAY <akshayk8714@gmail.com>
2025-10-14 15:01:37 +0530

    Modified function

--- Myapp/functions.py
+++ Myapp/functions.py
@@ -1,16 +1,24 @@
-FILEPATH = "todos.txt"
+#
+import os
 
 
-def get_todos(filepath=FILEPATH):
-    """ Read a text file and return the list of
-    to-do items.
-    """
+# def get_todos(filepath=FILEPATH):
+#     """ Read a text file and return the list of
+#     to-do items.
+#     """
+#     with open(filepath, 'r') as file_local:
+#         todos_local = file_local.readlines()
+#     return todos_local
+
+def get_todos(filepath="todos.txt"):
+    if not os.path.exists(filepath):
+        open(filepath, 'w').close()  # Create empty file if not exists
+        return []
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-
-def write_todos(todos_arg, filepath=FILEPATH):
+def write_todos(todos_arg, filepath="todos.txt"):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
